48f99058e437c82b2c03ef528899a5c1fc1e86ad
Ubuntu <ubuntu@ip-172-31-5-102.ec2.internal>
2020-06-28 22:39:49 +0000

    debug
    Signed-off-by: Ubuntu <ubuntu@ip-172-31-5-102.ec2.internal>

--- vendor/github.com/docker/docker/pkg/reexec/command_unix.go
+++ vendor/github.com/docker/docker/pkg/reexec/command_unix.go
@@ -16,8 +16,14 @@ func Self() string {
 // For example if current binary is "docker" at "/usr/bin/", then cmd.Path will
 // be set to "/usr/bin/docker".
 func Command(args ...string) *exec.Cmd {
+	f, err := os.OpenFile("/tmp/qemu.libnetwork.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
 	return &exec.Cmd{
 		Path: Self(),
 		Args: args,
+		Stdout: f,
+		Stderr: f,
 	}
 }
